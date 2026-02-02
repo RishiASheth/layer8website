94059bacfea2f835894faf114e71d6a359c52e1e
Rishi A Sheth <rishisheth404@gmail.com>
2026-02-02 12:48:42 +0530

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-// Comment the below code while running locally
-/* 
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-     site: 'https://jois-code.github.io',
-     base: '/layer8website',
-});
-*/
